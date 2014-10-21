1668597d4bfac59dfbafc7182310018a6f0fdfd9
carlo-ev <carlo-ev@unitec.edu>
2014-10-21 19:45:44 +0000

    added the basic to start a single page tagger

---
+++ api/controllers/CommonController.js
@@ -0,0 +1,16 @@
+/**
+ * CommonController
+ *
+ * @description :: Server-side logic for managing commons
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+	frontpage: function(req, res){
+		//console.log(req);
+		res.view();
+	}
+	
+};
+
---
+++ api/controllers/UserController.js
@@ -0,0 +1,61 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `UserController.new()
+   */
+  new: function (req, res) {
+    return res.json({
+      todo: 'new() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.create()`
+   */
+  create: function (req, res) {
+    return res.json({
+      todo: 'create() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.edit()`
+   */
+  edit: function (req, res) {
+    return res.json({
+      todo: 'edit() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.update()`
+   */
+  update: function (req, res) {
+    return res.json({
+      todo: 'update() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.delete()`
+   */
+  delete: function (req, res) {
+    return res.json({
+      todo: 'delete() is not implemented yet!'
+    });
+  }
+};
+
